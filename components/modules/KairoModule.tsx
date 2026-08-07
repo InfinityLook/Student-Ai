c8effa6186d2631512913aee59f9f9197d0ce861
InfinityLook <160535867+InfinityLook@users.noreply.github.com>
2026-08-07 12:53:50 +0200

    Fix formatting issue in KairoModule.tsx

--- components/modules/KairoModule.tsx
+++ components/modules/KairoModule.tsx
@@ -183,4 +183,4 @@ export default function KairoModule() {
       </div>
     </div>
   );
-      }
+                                                                                                                                              }
